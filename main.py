3f457a91f579b253153f6204673d96e17aa7bb13
JANEK-KIERAI <jangorgorjan@gmail.com>
2026-04-25 10:00:52 +0200

    main.py

--- main.py
+++ main.py
@@ -27,11 +27,40 @@ async def check(ctx):
     if ctx.message.attachments:
         for attachment in ctx.message.attachments:
             file_name = attachment.filename
-            await attachment.save(f'./{name}')
+            await attachment.save(f'./{file_name}')
             image = Image.open(file_name)
-            await ctx.send(get_class(image, "keras_model.h5", "labels.txt"))
+            class_name, confidence_score = get_class(image, "keras_model.h5", "labels.txt")
+            
+            if confidence_score > 0.75:
+                await ctx.send(f"To wygląda na: {class_name} śmietnik.")
+            else:
+                await ctx.send("Nie jestem pewien, co to jest. Spróbuj zrobić lepsze zdjęcie!")
     else:
         await ctx.send("NIE ZAŁĄCZYŁEŚ PLIKU!")
+        
+@bot.command()
+async def co2(ctx):
+    data = requests.get("https://global-warming.org/api/co2-api").json()
+    
+    najnowszy = data['co2'][-1]
+    
+    dzien = najnowszy['day']
+    miesiac = najnowszy['month']
+    rok = najnowszy['year']
+    wartosc = najnowszy['trend']
+    
+    await ctx.send(f"🌍 Aktualne stężenie CO2 (dane z {dzien}.{miesiac}.{rok}): **{wartosc} ppm**.")
+    
+@bot.command()
+async def mapa(ctx):
+    embed = discord.Embed(
+        title="🌍 Interaktywna Mapa Emisji CO2",
+        description="Kliknij poniżej, aby zobaczyć na żywo, skąd pochodzi prąd i ile CO2 emituje każdy kraj!",
+        color=0x2ECC71, # Zielony kolor
+        url="https://app.electricitymaps.com/map",
+    )
+    
+    await ctx.send(embed=embed, file = discord.File("M.png"))
 
 
 
